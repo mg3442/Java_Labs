2555fa962769a503749502aa4a1f322d522635fb
mg3442 <magnusprotas@gmail.com>
2018-12-26 14:54:08 +0300

    fix: add labs

---
+++ lab1/IDCard.java
@@ -0,0 +1,9 @@
+public class IDCard {
+	public static void main(String[] args){
+		System.out.println("\t Name and Surname: \t" + "Aliaksiej Protas");
+		System.out.println("\t Group number: \t\t" + "pois 1709v");
+		System.out.println("\t Primary activity:\t" + "work and study");
+		System.out.println("\t Hobbies: \t\t" + "music,traveling,films");
+		}
+	}
+		
